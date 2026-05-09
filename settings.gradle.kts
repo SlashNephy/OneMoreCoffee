a744a079834947877b60a201913e1a90e6e41798
SlashNephy <spica@starry.blue>
2026-05-09 15:22:21 +0900

    chore: Androidプロジェクトの土台を追加

---
+++ settings.gradle.kts
@@ -0,0 +1,45 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "OneMoreCoffee"
+
+include(
+    ":app",
+    ":core:common",
+    ":core:domain",
+    ":core:data",
+    ":core:ui",
+    ":feature:map",
+    ":feature:list",
+    ":feature:stats",
+    ":feature:settings",
+    ":feature:import",
+)
+
+listOf(
+    ":app",
+    ":core:common",
+    ":core:domain",
+    ":core:data",
+    ":core:ui",
+    ":feature:map",
+    ":feature:list",
+    ":feature:stats",
+    ":feature:settings",
+    ":feature:import",
+).forEach { project(it).projectDir.mkdirs() }
